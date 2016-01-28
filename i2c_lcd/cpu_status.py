bb2d1c60e013690f67647d29f5426beeef0f033e
zzeromin <zzeromin@gmail.com>
2016-01-28 16:01:02 +0900

    Update cpu_status.py

--- i2c_lcd/cpu_status.py
+++ i2c_lcd/cpu_status.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 # Original code found at:
 # https://gist.github.com/DenisFromHR/cc863375a6e19dce359d
-# modified by zzeromin
-
+# edited by zzeromin
 # requires I2C_LCD_driver.py
-
 
 import I2C_LCD_driver
 import os
-from time import sleep
+from time import *
 
 def get_cpu_temp():
         tempFile = open("/sys/class/thermal/thermal_zone0/temp")
@@ -26,7 +24,18 @@ mylcd = I2C_LCD_driver.lcd()
 
 mylcd.lcd_clear()
 
+old_Temp = new_Temp = get_cpu_temp()
+old_Speed = new_Speed = get_cpu_speed()
+
 while 1:
-	mylcd.lcd_display_string("CPU Temp: " + str( round( get_cpu_temp() ) ), 1)
-	mylcd.lcd_display_string("CPU Speed: " + str( round( get_cpu_speed() ) ), 2)
-	sleep(2)
+   new_Temp = get_cpu_temp()
+   new_Speed = get_cpu_speed()
+
+   if old_Temp != new_Temp or old_Speed != new_Speed :
+      old_Temp = new_Temp
+      old_Speed = new_Speed
+      print "CPU Temp: " + str( new_Temp )
+      print "CPU Speed: " + str( new_Speed )
+      mylcd.lcd_display_string("CPU Temp: " + str( new_Temp ), 1)
+      mylcd.lcd_display_string("CPU Speed: " + str( round( get_cpu_speed() ) ), 2)
+      sleep(1)
