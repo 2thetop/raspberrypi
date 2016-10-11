a9309ebef62609f0a98ce1bbc8de63fa112543c1
zzeromin <zzeromin@gmail.com>
2016-10-11 10:39:20 +0900

    Create retropie_info.py

---
+++ i2c_lcd/16x2/retropie_info.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# reference:  https://github.com/retropie/retropie-setup/wiki/runcommand
+# written by zzeromin, member of Raspberrypi Village
+# http://www.rasplay.org, http://forums.rasplay.org/
+# requires I2C_LCD_driver.py
+
+import I2C_LCD_driver
+from time import *
+
+mylcd = I2C_LCD_driver.lcd()
+mylcd.lcd_clear()
+
+while 1:
+   sec = 0
+   while ( sec < 5 ) :
+       f = open('/dev/shm/runcommand.log', 'r')
+
+       system = f.readline()
+       system = system.replace("\n","")
+       systemMap = {
+          "gba":"Game Boy Advance",
+          "mame-libretro":"MAME",
+          "msx":"MSX",
+          "fba":"FinalBurn Alpha",
+          "nes":"Nintendo Entertainment System"
+       }
+       system = systemMap.get(system)
+
+       rom = f.readline()
+       rom = rom.replace("\n","")
+
+       f.close()
+       
+       # show system & rom file information
+       mylcd.lcd_display_string( "%s" %(system), 1 )
+       mylcd.lcd_display_string( "%s" %(rom), 2 )
+       sec = sec + 1
+       sleep(1)
+
+   mylcd.lcd_clear()
